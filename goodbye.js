2a5aec7074c62b45fbe3f6e66f1ef9101e738ab2
Arun Dinesan <44499145+Arundinesan@users.noreply.github.com>
2020-08-13 14:30:45 +0530

    Add files via upload

---
+++ goodbye.js
@@ -0,0 +1,14 @@
+(function (window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = {};
+
+	// STEP 8: Rewrite the 'speak' function such that it is attached to the
+	// byeSpeaker object instead of being a standalone function.
+	// See Lecture 52, part 2
+	byeSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	}
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ hello.js
@@ -0,0 +1,13 @@
+(function (window) {
+	var speakWord = "Hello";
+	var helloSpeaker = {};
+	// STEP 4: Rewrite the 'speak' function such that it is attached to the
+	// helloSpeaker object instead of being a standalone function.
+	// See Lecture 52, part 2
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window)
